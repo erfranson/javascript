b82749ae82ba50a02a1ff7b8a7631d725cc5cbe7
Erfranson <Ethanfranson2@gmail.com>
2019-03-13 01:10:33 -0600

    adding more content

--- script.js
+++ script.js
@@ -1,1 +1,66 @@
-console.log('hello world');
+// ****Variables
+
+// var firstName = 'Ethan';
+// var age = 30;
+//
+//
+// var lastName = 'Franson';
+//
+// var fullAge = true;
+//
+// var job;
+//  job = 'teacher';
+// console.log(firstName + " " +lastName);
+
+/**********
+* Variable mutation and type coercion
+*/
+
+
+// var firstName = 'john';
+// var age = 30;
+
+// **type coercion changes data types to match other data types
+
+// console.log(firstName + ' ' + age);
+//
+// var job, isMarried;
+//
+// job = 'teacher';
+// isMarried = false;
+//
+// console.log(firstName + ' is a ' + age + ' year old ' + job + '! Is he married? ' + isMarried );
+//
+// // varible mutation
+//
+// age = 'twenty eight';
+// job = 'driver';
+//
+// alert(age);
+//
+// var lastName = prompt('What is his last name?');
+// console.log(firstName + " "  + lastName);
+
+/*
+ Basic Operators
+*/
+
+var year, yearJohn, yearMark;
+
+now = 2019;
+ageJohn = 28;
+ageMark = 33;
+
+// Math operators
+yearJohn = now - ageJohn;
+yearMark =  now - ageMark;
+
+console.log(yearJohn);
+
+console.log(now * 2);
+console.log(now + 2);
+console.log(now / 10);
+
+// logical operators
+var johnOlder = ageJohn < ageMark;
+console.log(johnOlder);
