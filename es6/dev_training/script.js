0471930402a3411678d73afbd9776e8e74a85b7b
Ethan Franson <ethanfranson2@gmail.com>
2019-12-12 11:31:58 -0700

    dev training

---
+++ es6/dev_training/script.js
@@ -0,0 +1,23 @@
+//Let_Const
+
+
+
+
+// arrow functions lexical_this
+
+// arrays
+
+//destructuring 
+
+//maps
+
+//rest_params spread_operator, default_params
+
+
+//classes 
+
+//promises
+
+//async await
+
+//block iifes
